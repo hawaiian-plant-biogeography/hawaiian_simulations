9a8d795138bc4bffa341a5870f2fa8708de072f1
Swiston <k.swiston@compute1-client-1.ris.wustl.edu>
2023-11-17 18:14:31 -0600

    batch script

--- scripts/batchsim.sh
+++ scripts/batchsim.sh
@@ -3,7 +3,7 @@ LSF_DOCKER_VOLUMES="/storage1/fs1/michael.landis/Active/hawaiian_simulations:/st
 JOBDIR="/storage1/fs1/michael.landis/Active/hawaiian_simulations/joblogs"
 
 # Create a list of sims to run (numbers)
-RUN_LIST=(1 2 3 4 5 6 7 8 9 10)
+RUN_LIST=$(seq 1 100)
 
 # Create and run a job for each sim
 for i in ${RUN_LIST[@]}
@@ -14,6 +14,6 @@ do
 	-o $JOBDIR/$i.stdout.txt \
 	-J $i \
 	-q general \
-	-n 3 -M 15GB -R "rusage [mem=15GB] span[hosts=1]" \
+	-n 4 -M 16GB -R "rusage [mem=16GB] span[hosts=1]" \
 	-a 'docker(sswiston/rb_tp:test)' /bin/bash /storage1/fs1/michael.landis/Active/hawaiian_simulations/scripts/sim.sh
 done
