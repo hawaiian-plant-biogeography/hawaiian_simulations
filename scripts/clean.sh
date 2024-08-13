3c919502a887870902666492770c654da5dff138
Michael Landis <mlandis@gmail.com>
2024-08-13 11:47:00 -0500

    add clean script

---
+++ scripts/clean.sh
@@ -0,0 +1,5 @@
+rm experiment1/sim_data/*.*
+rm experiment1/model_truth/**
+rm experiment1/geosse_rates_for_pj_scripts/*.*
+rm experiment1/pj_scripts_generated_in_py/*.*
+rm experiment1/pj_output/*.*
